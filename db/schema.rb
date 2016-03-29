0e3dd9aebb3bf1c9dc31fb295c781a52b3a3489a
Dodi Sabaruddin <ddsbrddn@gmail.com>
2016-03-29 21:35:20 +0700

    modified database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@
 ActiveRecord::Schema.define(version: 20160305191258) do
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title",      limit: 255
+    t.text     "body",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
